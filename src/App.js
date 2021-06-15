61d470dc15c769ec3a04ff2205ab975d783969c3
Carlos Perez <cperezwuait@gmail.com>
2021-06-15 18:23:21 +0200

    nivel 2

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import Obra from './obra.json'
 import Linia from './components/linia'
+import styled from 'styled-components'
+import {Container} from './styles/styled'
 
   function App() {
 
     const linias = Obra.map( (e) => <Linia value={e} key={e.id}/>);
   
     return (
-      <div className="App">
-  
+      <div>
+        <Container>
         {linias}
+        </Container>
         
       </div>
     );
---
+++ src/styles/styled.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    width: 700px;
+    background-color: black;
+    color: white;
+    padding: 20px;
+    margin:auto;
+    margin-top: 50px;
+`;
